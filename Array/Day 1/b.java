af04a3963fd0aabf3db181cb4894cc85cac41c2d
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-05 00:09:09 +0530

    Way to add element in Array

---
+++ Array/Day 1/b.java
@@ -0,0 +1,9 @@
+class b {
+    public static void main(String[] args) {
+        int b[] = {12, 20, 30, 30, 40, 50};
+        for (int i=0; i<=b.length-1; i++){
+            System.out.println(b[i]);
+        }
+
+    }
+}
