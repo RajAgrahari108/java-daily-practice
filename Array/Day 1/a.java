c6b1cc494a9d8958669b56102c6deee3110340ac
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-04 23:39:43 +0530

    Commiting all chnages

---
+++ Array/Day 1/a.java
@@ -0,0 +1,15 @@
+public class a {
+    public static void main(String args[]){
+        int a[] = new int[5];
+        a[0] = 85;
+        a[1] = 90;
+        a[2] = 95;
+        a[3] = 88;
+        a[4] = 92;
+
+        for(int i=0; i<=a.length-1; i++ ){
+            System.out.println(a[i]);
+        }
+ 
+    }
+}
