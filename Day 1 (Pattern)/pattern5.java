1ad2f9562774de806a68c13d5b125fee39c81338
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-04 23:14:25 +0530

    Adding

---
+++ Day 1 (Pattern)/pattern5.java
@@ -0,0 +1,12 @@
+public class pattern5 {
+   public static void main(String[] args) {
+        int x = 1;
+        for (int i=1; i<=5 ; i++){
+            for (int j =1; j<=5 ;j++){
+                System.out.printf("%3d",x);
+                x++;
+            }
+            System.out.println();
+        }
+    } 
+}
