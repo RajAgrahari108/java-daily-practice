04f06529051e302d5c7fd8d2454f68584ce5e13f
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-05 22:10:43 +0530

    sumOfOddElement

--- Array/Day 1/sumOfOddElement.java
+++ Array/Day 1/sumOfOddElement.java
@@ -4,7 +4,7 @@ public class sumOfOddElement {
         int sum = 0 ;
         for (int i=0; i<=a.length-1; i++) {
             if(a[i] % 2 !=0){
-                sum = sum ;
+                sum = sum + a[i];
                 System.out.println(sum);
             }
         }
