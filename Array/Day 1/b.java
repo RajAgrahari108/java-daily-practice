e0552a16259389de766a117b7cfa17942bd0bc27
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-05 22:50:35 +0530

    updating the vlaues of for loop iondtant of (i<=b.length-1) to (i<b.length)

--- Array/Day 1/b.java
+++ Array/Day 1/b.java
@@ -1,7 +1,7 @@
 class b {
     public static void main(String[] args) {
         int b[] = {12, 20, 30, 30, 40, 50};
-        for (int i=0; i<=b.length-1; i++){
+        for (int i=0; i<b.length; i++){
             System.out.println(b[i]);
         }
 
