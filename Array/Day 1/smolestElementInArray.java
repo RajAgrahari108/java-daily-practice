47d3cf060760a7b16402d0feaf85dadf3267d62a
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-05 22:52:15 +0530

    print a java program smolest Element In Array

---
+++ Array/Day 1/smolestElementInArray.java
@@ -0,0 +1,13 @@
+public class smolestElementInArray {
+    public static void main(String args[]){
+
+        int a[] = {10,222,3,40,5,900,100};
+        int num = 0;
+        for(int i=0; i<a.length; i++) {
+            if(num < a[i]){
+              num = a[i];
+           }
+        }
+        System.out.println(num);
+    }  
+}
