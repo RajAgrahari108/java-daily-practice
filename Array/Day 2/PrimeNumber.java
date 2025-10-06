b06b1acec4a8279d458f16364100292b77f2e987
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-06 22:58:41 +0530

    PrimeNumber

---
+++ Array/Day 2/PrimeNumber.java
@@ -0,0 +1,12 @@
+
+
+public class PrimeNumber {
+     public static void main(String[] args) {
+        int b[] = {4, 7, 10, 15, 17, 20, 23};
+        for (int i=0; i<b.length; i++){
+            if(b[i] %2==0 & b[i]   )
+            System.out.println(b[i]);
+        }
+
+    }
+}
