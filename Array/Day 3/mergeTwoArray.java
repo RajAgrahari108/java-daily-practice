2ee29af836537a8b0dc5f50e9bb69461d95004a0
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-10 23:09:10 +0530

    Adding

---
+++ Array/Day 3/mergeTwoArray.java
@@ -0,0 +1,8 @@
+public class mergeTwoArray {
+    public static void main(String[] args) {
+        int a[]={12, 23, 45, 56};
+        int b[]={120, 230, 450,560};
+        
+        System.out.println(a[] + b[]);
+    }
+}
