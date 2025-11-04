3b30c4060ce9f606badab1b9a7216adda95777e1
Raj Agrahari <raj.agr2002@gmail.com>
2025-11-05 00:37:37 +0530

    updating Sum Of Digit

--- String/Day 2/SumOfDigit.java
+++ String/Day 2/SumOfDigit.java
@@ -6,7 +6,7 @@ public class SumOfDigit {
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(ch >= '0' && ch<='9'){
-                sum = sum+ (ch-'0');
+                sum = sum+ (ch-'0);
             }
         }
         System.out.println(sum);
