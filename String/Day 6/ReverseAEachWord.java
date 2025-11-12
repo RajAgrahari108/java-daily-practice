7388f08c5afb883e0b1593ba3d6e1b2fd86c66cc
Raj Agrahari <raj.agr2002@gmail.com>
2025-11-13 00:01:42 +0530

    Adding

---
+++ String/Day 6/ReverseAEachWord.java
@@ -0,0 +1,13 @@
+public class ReverseAEachWord {
+    public static void main(String[] args) {
+       
+       String s = "RAJ IS GOOD";
+       String a[] = s.split("");
+       String  rev = " ";
+        for(int i=0; i<s.length(); i++){
+           
+            
+        }
+       
+    }
+}
--- String/day 1/reverse.java
+++ String/day 1/reverse.java
@@ -19,3 +19,28 @@ public class reverse {
     return new String(ch);
    }
 }
+
+
+// ! using function
+// public class String345 {
+//     public static void main(String[] args) {
+       
+//         char[] s = {'h', 'e', 'l', 'l', 'o'};
+//         Solution sol = new Solution();
+//         sol.reverseString(s);
+//         System.out.println(s);
+//     }
+// }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int i = 0, j = s.length - 1;
+//         while (i < j) {
+//             char temp = s[i];
+//             s[i] = s[j];
+//             s[j] = temp;
+//             i++;
+//             j--;
+//         }
+//     }
+// }
